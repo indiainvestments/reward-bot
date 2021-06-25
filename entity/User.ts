255b1731c78c334c90603368c7058d699e022fc6
Dwight Schrute <84026436+dwight-schrute@users.noreply.github.com>
2021-06-25 12:01:38 +0530

    feat: postgres db conf

---
+++ entity/User.ts
@@ -0,0 +1,18 @@
+import {BaseEntity, Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    isActive: boolean;
+
+}
