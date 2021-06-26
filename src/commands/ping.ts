a028e50d719278607034154edc43ee028d8915b1
Dwight Schrute <84026436+dwight-schrute@users.noreply.github.com>
2021-06-26 12:36:07 +0530

    feat: setup init schema for rewarding

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,5 +1,4 @@
 import { Command } from 'discord-akairo';
-import Repository  from '../repository/Repository';
 import { Message } from 'discord.js';
 
 class PingCommand extends Command {
@@ -10,12 +9,7 @@ class PingCommand extends Command {
     }
 
     async exec(message: Message) {
-        const list = await Repository.getAllUsers();
-        const ll = list.map(u => {
-            return `${u.firstName} ${u.lastName}`;
-        })
-        console.log("info list all", ll);
-        return message.reply('Pong!' + ll);
+        return message.reply('Pong!');
     }
 }
 
--- src/commands/reward.ts
+++ src/commands/reward.ts
@@ -1,5 +1,6 @@
 import { CommandUtil, Command } from 'discord-akairo';
 import { Message, User } from 'discord.js';
+import Repository from '../repository/Repository';
 
 class ThanksCommand extends Command {
     constructor() {
@@ -8,25 +9,32 @@ class ThanksCommand extends Command {
       });
     }
     private thanksTriggers = [
-      'thanks',
-      'thank you',
-      'thxs',
-      'ty',
-      'tq'
+        'thanks',
+        'thank you',
+        'thxs',
+        'ty',
+        'tq'
     ];
 
     regex = new RegExp('\\b' + this.thanksTriggers.join('\\b|\\b') + '\\b', 'i');
 
     exec(message: Message) {
-      const { channel, mentions } = message;
-      mentions.users.forEach((user: User): void => {
-        // add checks like - can't award self
-        // and give reward to relevant mentions
-      });
-
-      return channel.send(
-        CommandUtil.transformOptions(`Rewarded ${mentions.users.map((user: User) => user)}`),
-      );
+        const { channel, mentions, author } = message;
+        console.log("reward", channel);
+        if (mentions.users.size <= 0) return;
+        const rewardees: User[] = [];
+        mentions.users.forEach((user: User): void => {
+            // add checks like - can't award self
+            // and give reward to relevant mentions
+            if (user.id !== author.id) {
+                rewardees.push(user);
+            }
+        });
+        if (rewardees.length <= 0) return;
+        Repository.saveRewardEvent(author, rewardees, channel.id);
+        return channel.send(
+            CommandUtil.transformOptions(`Rewarded ${rewardees.map((user: User) => user)}`),
+        );
     }
 }
 
